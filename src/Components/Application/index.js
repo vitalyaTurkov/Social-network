f88abd6911a57be3f93abb83c7a9dad1b4a619bd
vitalyaTurkov <vitaliatyrkov@yandex.ru>
2018-10-23 15:26:55 +0700

    Added the ability to search for registered users.

--- src/Components/Application/index.js
+++ src/Components/Application/index.js
@@ -5,30 +5,70 @@ import AboutPerson from '../AboutPerson'
 import AppBar from '../AppBar'
 import PersonPhoto from '../PersonPhoto'
 import MainMenu from '../MainMenu'
+import { classes } from './styles'
+import { getUserById } from "../../service/api/api";
 
 import Grid from '@material-ui/core/Grid'
+import { withStyles } from '@material-ui/core'
 
-class Application extends React.Component
-{
-    render()
-    {
-        if(!this.props.isAuthorized)
-        {
-            return (
-                <Redirect to={'/authorization'}/>
-            )
+class Application extends React.Component {
+
+    state = {
+        name: "",
+        surname: "",
+        id: this.props.id,
+        redirect: false
+    };
+
+    componentWillMount() {
+        if(!this.props.isAuthorized) return;
+
+        const {pathname} = window.location;
+        const id = pathname.substring(6);
+        if(!this.state.redirect) {
+            getUserById(id)
+                .then(res => this.setState({
+                    name: res.data.name,
+                    surname: res.data.surname,
+                    id: res.data.id,
+                }));
         }
+    }
+
+    componentWillReceiveProps() {
+        if(!this.props.isAuthorized) return;
+        const {pathname} = window.location;
+        const id = pathname.substring(6);
+        if(!this.state.redirect) {
+            getUserById(id)
+                .then(res => this.setState({
+                    name: res.data.name,
+                    surname: res.data.surname,
+                    id: res.data.id,
+                }));
+        }
+    }
+
+    onSelectSearch = (id) => {
+        this.setState({...this.state, id: id, redirect: false});
+    };
+
+    render() {
+        const {classes, menuIsOpened, openMenuAction} = this.props;
+
+        if (!this.props.isAuthorized) return <Redirect to={'/authorization'}/>;
+
         return (
             <div>
-                <AppBar openMenuAction={this.props.openMenuAction}/>
-                <Grid container>
+                <AppBar onSelect={this.onSelectSearch} openMenuAction={openMenuAction}/>
+                <Grid container className={classes.container}>
                     <PersonPhoto url={'https://img1.goodfon.ru/wallpaper/big/1/b1/fairy-tail-erza-elza-devushka.jpg'}/>
-                    <AboutPerson name={this.props.name} surname={this.props.surname}/>
-                    <MainMenu isOpen={this.props.menuIsOpened} openMenuAction={this.props.openMenuAction}/>
+                    <AboutPerson name={this.state.name} surname={this.state.surname}/>
+                    <MainMenu isOpen={menuIsOpened} openMenuAction={openMenuAction}/>
                 </Grid>
             </div>
         )
     }
 }
 
-export default Application
+export default withStyles(classes)(Application)
---
+++ src/Components/Application/styles.js
@@ -0,0 +1,7 @@
+export const classes = {
+    container: {
+        marginTop: "20px",
+        maxWidth: "800px",
+        margin: "0 auto"
+    }
+};
