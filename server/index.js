7aaa39fa23968a53c7965d890102d55f038b95cc
vitalyaTurkov <vitaliatyrkov@yandex.ru>
2018-10-23 15:22:13 +0700

    New route - get user by id.

--- server/index.js
+++ server/index.js
@@ -4,7 +4,6 @@ const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
-
 app.use(cors());
 app.use(bodyParser.urlencoded({
     extended: true
--- server/pages/users-routes.js
+++ server/pages/users-routes.js
@@ -9,11 +9,22 @@ module.exports = function(app) {
             password: password,
             email: email
         });
+        res.send("OK");
     });
 
     app.get('/user', (req, res) => {
+        if(req.query.id) {
+            db.getUserById(req.query.id)
+                .then(user => res.send(user));
+            return;
+        }
         db.getUser(req.query.email, req.query.password, (user) => {
             res.send(user);
         });
+    });
+
+    app.get('/users', (req, res) => {
+        db.getUsers()
+            .then(users => res.send(users));
     });
 };
